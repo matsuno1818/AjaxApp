f3b865f4a435ea2037fff7da5897f66c6e96a40d
matsuno1818 <matsuno.812@gmail.com>
2020-07-31 15:30:32 +0900

    ビューファイルの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,12 @@
 class PostsController < ApplicationController
-
-  def index 
-    @posts = Post.all.order(id: "DESC")  # すべてのレコードを@postに代入
+  def index
+    @posts = Post.all.order(id: "DESC")
   end
-  
 
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
-  end  
+  end
 
   def checked
     post = Post.find(params[:id])
