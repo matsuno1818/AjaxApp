dc242002778bbaa4234f0c7ea8608d67ab104d58
matsuno1818 <matsuno.812@gmail.com>
2020-07-31 12:43:49 +0900

    実装の準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,13 @@
 class PostsController < ApplicationController
 
   def index 
-    @posts = Post.all  # すべてのレコードを@postに代入
+    @posts = Post.all.order(id: "DESC")  # すべてのレコードを@postに代入
   end
   
-  def new
-  end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end  
 
 end
