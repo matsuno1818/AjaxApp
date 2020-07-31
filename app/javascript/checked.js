cd8d2c7137692fcf2fac8b3aac39cc9f364a939d
matsuno1818 <matsuno.812@gmail.com>
2020-07-31 14:35:19 +0900

    XMLHttpRequestを使用できるように実装

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -3,8 +3,13 @@ function check() {
   postsA = Array.from(posts);
 
   postsA.forEach(function (post) {
-    post.addEventListener("click", (e) => { });
+    post.addEventListener("click", (e) => {
       const postId = post.getAttribute("data-id");
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `/posts/${postId}`, true);
+      XHR.responseType = "json";
+      XHR.send();
+    });
   });
 }
 window.addEventListener("load", check);
